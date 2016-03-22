223a0a748ab60770d4cd81b01e49766dfa0cc864
Abel <abelabadi2@gmail.com>
2016-03-22 16:23:20 -0500

    SinglyLinkedList skeleton addNode,removeNode,search and printNodes

---
+++ SinglyLinkedLIst/SinglyLinkedListWithHeader.java
@@ -0,0 +1,30 @@
+public class SinglyLinkedListWithHeader {
+	
+	Node header = null; //contains no data, cannot be removed
+
+	public SinglyLinkedListWithHeader() {
+		header = new Node();
+	}
+
+	void addNode(String s) {
+			
+	}
+	
+	
+	void removeNode(String s) {
+
+	}	
+
+	boolean search(String s) {
+		
+	}
+	
+	void printNodes() {
+		
+	}
+	
+	
+	
+}
+
+
