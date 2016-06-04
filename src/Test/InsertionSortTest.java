f251cb3b1ca084b6af139bf9168300512f7c31e9
abel abadi <abelabadi2@gmail.com>
2016-06-04 18:10:42 -0500

    setup junit4 for testing

---
+++ src/Test/InsertionSortTest.java
@@ -0,0 +1,14 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by abelo on 6/4/2016.
+ */
+public class InsertionSortTest {
+    @org.junit.Test
+    public void insertionSort() throws Exception {
+
+    }
+
+}
---
+++ src/insertionSort.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+//insertion sort
+class InsertionSort{
+
+	public ArrayList<Integer> insertionSort(ArrayList<Integer> list){
+
+		return null;
+	}
+}
