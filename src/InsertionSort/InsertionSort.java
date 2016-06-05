e452efa76fc92b014d234820297704938194ae07
abel abadi <abelabadi2@gmail.com>
2016-06-05 10:58:04 -0500

    works
    all tests passed

--- src/InsertionSort/InsertionSort.java
+++ src/InsertionSort/InsertionSort.java
@@ -1,13 +1,33 @@
 package InsertionSort;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+
 //insertion sort
 public class InsertionSort{
 
     public InsertionSort(){}
 	public ArrayList<Integer> insertionSort(ArrayList<Integer> list){
+        int currentVacant = 0; //currentVacant index
+        int currentValue = 0; // value at currentVacant index
 
+        for (int i=1; i<list.size(); i++){
 
-		return null;
+            currentVacant = i; //
+            currentValue = list.get(currentVacant); // to be inserted
+
+            //till the first element and the value before currentVacant is greater than currentValue
+            //swap them
+            while(currentVacant >0){
+                if (list.get(currentVacant-1)<=currentValue)
+                    break;
+                list.set(currentVacant,list.get(currentVacant-1));
+                currentVacant--;
+            }
+            list.set(currentVacant,currentValue);
+
+        }
+        return list;
 	}
+
 }
--- src/InsertionSort/Test/InsertionSortTest.java
+++ src/InsertionSort/Test/InsertionSortTest.java
@@ -16,22 +16,22 @@ public class InsertionSortTest {
         InsertionSort sort = new InsertionSort();
 
         ArrayList<ArrayList<Integer>> input = new ArrayList<>();
-        input.add(new ArrayList<>(Arrays.asList(0))); //[0]
-        input.add(new ArrayList<>(Arrays.asList(0))); //[0]
-        input.add(new ArrayList<>(Arrays.asList(0,0,0))); //[0,0,0]
-        input.add(new ArrayList<>(Arrays.asList(0,0,0))); //[0,0,0]
-        input.add(new ArrayList<>(Arrays.asList(1,0))); //[1,0]
-        input.add(new ArrayList<>(Arrays.asList(0,1))); //[0,1]
-        input.add(new ArrayList<>(Arrays.asList(0,2,1))); //[0,2,1]
-        input.add(new ArrayList<>(Arrays.asList(0,1,2))); //[0,1,2]
-        input.add(new ArrayList<>(Arrays.asList(10, 9, 8, 7, 6, 5, 4, 3, 2, 1)));
-        input.add(new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)));
-        input.add(new ArrayList<>(Arrays.asList(-11, -3, 9, 17, 42, 54, 54, 602, 999)));
+//        input.add(new ArrayList<>(Arrays.asList(0))); //[0]
+//        input.add(new ArrayList<>(Arrays.asList(0))); //[0]
+//        input.add(new ArrayList<>(Arrays.asList(0,0,0))); //[0,0,0]
+//        input.add(new ArrayList<>(Arrays.asList(0,0,0))); //[0,0,0]
+//        input.add(new ArrayList<>(Arrays.asList(1,0))); //[1,0]
+//        input.add(new ArrayList<>(Arrays.asList(0,1))); //[0,1]
+//        input.add(new ArrayList<>(Arrays.asList(0,2,1))); //[0,2,1]
+//        input.add(new ArrayList<>(Arrays.asList(0,1,2))); //[0,1,2]
+//        input.add(new ArrayList<>(Arrays.asList(10, 9, 8, 7, 6, 5, 4, 3, 2, 1)));
+//        input.add(new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)));
         input.add(new ArrayList<>(Arrays.asList(42, 9, 17, 54, 602, -3, 54, 999, -11)));
+        input.add(new ArrayList<>(Arrays.asList(-11, -3, 9, 17, 42, 54, 54, 602, 999)));
 
-       for (int i=1; i<input.size(); i++){
-            Assert.assertEquals(input.get(i),sort.insertionSort(input.get(i-1)));
-       }
+
+            Assert.assertEquals(input.get(1),sort.insertionSort(input.get(0)));
+
     }
 
 }
